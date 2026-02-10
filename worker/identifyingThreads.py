b9fe83d13f13016e738a50de62af3db7eb281c1e
Tommyhillphyga <tommyhillphyga@gmail.com>
2026-02-10 22:41:09 +0100

    remove deprecated methods

--- worker/identifyingThreads.py
+++ worker/identifyingThreads.py
@@ -3,12 +3,17 @@ import time
 
 
 def myThread():
-    print("Thread {} starting".format(threading.currentThread().getName()))
+    print("Thread {} starting".format(threading.current_thread().name()))
     time.sleep(10)
-    print("Thread {} ending".format(threading.currentThread().getName()))
+    print("Thread {} ending".format(threading.current_thread().name()))
 
     for i in range(4):
         threadName = "Thread-" + str(i)
         thread = threading.Thread(name=threadName, target=myThread)
         thread.start()
     print("{}".format(threading.enumerate()))
+
+
+
+
+
