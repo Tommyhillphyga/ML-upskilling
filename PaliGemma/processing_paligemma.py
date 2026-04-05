8e9d8d477d878807e5341fdb45165cc8a59c401e
Tommyhillphyga <tommyhillphyga@gmail.com>
2026-04-05 12:20:46 +0100

    added image rescaling utility

--- PaliGemma/processing_paligemma.py
+++ PaliGemma/processing_paligemma.py
@@ -17,14 +17,15 @@ def resize(
         (width, height), resample=resample, reducing_gap=reducing_gap)
     return resized_image
 
-
-
-
-    
-
-
-
-
+def rescale(
+        image: np.ndarray, 
+        scale: float, 
+        dtype: np.dtype = np.float32,
+        
+) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
 
 
 def process_images(
