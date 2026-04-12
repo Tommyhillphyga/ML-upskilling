a4d11e06069a1721b10aba81368ccf3d8987f495
Tommyhillphyga <tommyhillphyga@gmail.com>
2026-04-12 15:19:15 +0100

    update the Gemma rotatry embedding

--- PaliGemma/modeling_gemma.py
+++ PaliGemma/modeling_gemma.py
@@ -258,6 +258,37 @@ class GemmaAttention(nn.Module):
           return attn_output, attn_weights
 
 
+class GemmaRotaryEmbedding(nn.Module):
+     def __init__(self, dim, max_position_embeddings = 2048, base = 10000, device = None):
+          super().__init__()
+
+          self.dim = dim
+          self.max_position_embeddings = max_position_embeddings
+          self.base = base
+
+          # calculate the theta according to tht formular theta_i = base^(2i/dim) where i is the index of the head dimension[0 , 1,2,...dim//2]
+
+          inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float() / self.dim))
+          self.register_buffer("inv_freq", tensor= inv_freq, persistent=False)
+
+
+     @torch.no_grad()
+     def forward(self, x, position_ids, seq_len=None):
+          #x: [batch_size, num_attention_heads, seq_len, head_size]
+          self.inv_freq.to(x.device)
+          # copy the inv_freq tensor for batch in the sequence
+          # inv_freq_expanded: [batch_size, head_dim // 2, 1]
+          inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+          # position_ids_expanded: [Batch_size, 1, seq_len]
+          position_ids_expanded = position_ids[:, None, :].float()
+          device_type = x.device.type
+          device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+
+          with torch.autocast(device_type=device_type, enabled=False):
+               # multiply each theta by the position (which is the argment of the sin and cos function)
+               # freqs: [batch_size, head_dim, // 2. 1] @ [batch_size, 1, seq_len] --> [batch_size, seq_len, head_dim // 2]
+               
+
 
 class GemmaDecoderLayer(nn.Module):
      def __init__(self, config: GemmaConfig, layer_idx: int):
