ce8ed6a18cf74acc17ba33877f0cc8ece4cc7a98
Tommyhillphyga <tommyhillphyga@gmail.com>
2026-04-15 17:24:32 +0100

    Add get model input func

--- PaliGemma/inference.py
+++ PaliGemma/inference.py
@@ -8,6 +8,17 @@ from processing_paligemma import PaliGemmaProcessor
 from utils import load_hf_model
 
 
+def get_model_input(
+        processor: PaliGemmaProcessor, prompt: str, image_file_path: str, device: str
+): 
+    image = Image.open (image_file_path)
+    images = [image]
+    prompts = [prompt]
+    model_inputs = processor(text=prompts, images = images)
+    model_inputs - move_inputs_to_device(model_inputs, device)
+    return model_inputs
+
+
 def test_inference(
         model: PaliGemmaForConditionalGeneration,
         processor: PaliGemmaProcessor,
@@ -15,7 +26,7 @@ def test_inference(
         prompt: str,
         image_file_path: str,
         max_tokens_to_generate: int,
-        temperature: float,
+        temperature: float, 
         top_p: float,
         do_sample: bool,
 
@@ -27,7 +38,51 @@ def test_inference(
     pixel_values = model_inputs["pixel_values"]
 
     kv_cache = KVCache()
-    
+
+    # Generate tokens untill you see the stop token 
+    stop_token = processor.tokenizer.eos_token_id
+    generated_tokens = []
+
+    for _ in range(max_tokens_to_generate):
+        outputs = model(
+            input_ids=input_ids,
+            pixel_values=pixel_values,
+            attention_mask=attention_mask,
+            kv_cache=kv_cache,
+
+        )
+        kv_cache = outputs["kv_cache"]
+        next_token_logits = outputs["logits"][:, -1, :]
+
+        if do_sample:
+            # Apply temperature
+            next_token_logits = torch.softmax(next_token_logits / temperature, dim=-1)
+            next_token = _sample_top_p(next_token_logits, top_p)
+
+        else:
+            next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)
+        assert next_token.size() == (1, 1)
+        next_token = next_token.squeeze(0) # Remove batch dimension
+        generated_tokens.append(next_token)
+        # stop if the stop token has been generated
+        if next_token.item() == stop_token:
+            break
+        # Append the next token to the input
+        input_ids = next_token.unsqueeze(-1)
+        attention_mask = torch.cat(
+            [attention_mask, torch.ones((1,1), device=input_ids.device)], dim=-1
+        )
+
+        generated_tokens = torch.cat(generated_tokens, dim=-1)
+        # Decode the genrated tokens
+
+        decoded = processor.tokenizer.decode(generated_tokens, skip_special_tokens = True)
+
+        print (prompt + decoded)
+
+
+
+
    
 
 
